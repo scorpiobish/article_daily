c1817b414ddcb9ed0014df77728734ee5ef128f9
scorpiobish <chizeanancy@gmail.com>
2023-02-02 17:24:53 +0100

    Update Splash Scrren.

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,67 @@ import 'package:google_fonts/google_fonts.dart';
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-     MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xfffedbd0),
-        body: Center(child: Text('Article\nDaily',
-          style: GoogleFonts.spirax(
-            textStyle: TextStyle(fontSize: 55),
-          ),
-          textAlign: TextAlign.center,
-        ),) ,
-      ),
+    MaterialApp(
+      home: Splash(),
     ),
   );
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffffffff),
+      body: SafeArea(
+        child: Align(
+          alignment: Alignment.topLeft,
+          child: Text(
+            'Hi Nancy,',
+            style: GoogleFonts.nunito(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Splash extends StatelessWidget {
+  const Splash({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(
+            'assets/background.png',
+          ),
+          colorFilter: ColorFilter.mode(
+            Colors.black.withOpacity(27/100),
+            BlendMode.srcOver,
+          ),
+          fit: BoxFit.cover,
+        )),
+        child: Center(
+          child: Text(
+            'ARTICLE\nDAILY',
+            style: GoogleFonts.quicksand(
+              textStyle: TextStyle(
+                letterSpacing: 7.5,
+                fontSize: 50,
+                fontWeight: FontWeight.w700,
+                height: 60 / 50,
+                color: Color.fromARGB(255, 255, 255, 255),
+              ),
+            ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
 }

2404d09bfebfe998c8e37a807410192343e57e52
scorpiobish <chizeanancy@gmail.com>
2023-03-01 17:40:48 +0100

    Implement News Card on Home screen

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -6,13 +5,96 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xffffffff),
-      body: SafeArea(
-        child: Align(
-          alignment: Alignment.topLeft,
-          child: Text(
-            'Hi, Nancy',
-            style: GoogleFonts.nunito(),
+      backgroundColor: Color(0xffF5F8FA),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Align(
+                alignment: Alignment.topLeft,
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Hi, ',
+                    style: GoogleFonts.quicksand(
+                      textStyle: TextStyle(
+                          fontSize: 36,
+                          fontWeight: FontWeight.w600,
+                          height: 45 / 36,
+                          color: Colors.black),
+                    ),
+                    children: const <TextSpan>[
+                      TextSpan(
+                        text: 'Nancy',
+                        style: TextStyle(
+                          color: Color(0xff0665FF),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 25.0,
+              ),
+              Container(
+                padding: EdgeInsets.only(
+                    left: 25.0, right: 25.0, top: 50.0, bottom: 50.0),
+                decoration: BoxDecoration(
+                  color: Color(0xff011B45),
+                  borderRadius: BorderRadius.circular(30.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.blueGrey.withOpacity(0.5),
+                      spreadRadius: 2.0,
+                      blurRadius: 3.0,
+                      offset: Offset(1, 2),
+                    ),
+                  ],
+                ),
+                child: Column(
+                  children: [
+                    Text(
+                      'Elon Musk is planning a 24-hour Tesla restaurant in Hollywood',
+                      style: GoogleFonts.quicksand(
+                        textStyle: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w600,
+                            height: 25 / 20,
+                            color: Colors.white),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row( mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Kate Krader',
+                          style: GoogleFonts.quicksand(
+                            textStyle: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                height: 17.5 / 14,
+                                color: Colors.white),
+                          ),
+                        ),
+                        Text(
+                          'Sent Today',
+                          style: GoogleFonts.quicksand(
+                            textStyle: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w500,
+                                height: 17.5 / 14,
+                                color: Colors.white.withOpacity(0.60)), 
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),

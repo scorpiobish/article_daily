7b2139b76d7a9aa0013d4a43c13cadba30cbb562
scorpiobish <chizeanancy@gmail.com>
2023-07-11 05:27:17 +0100

    set up saved screen

--- lib/saved.dart
+++ lib/saved.dart
@@ -27,6 +27,81 @@ class Saved extends StatelessWidget {
                     ),
                   ),
                 ),
+                SizedBox(
+                  height: 27.0,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    padding: EdgeInsets.only(
+                        left: 20.0, top: 16.0, right: 49.0, bottom: 16.0),
+                    width: 243,
+                    height: 52,
+                    child: Text(
+                      'Search saved article...',
+                      style: GoogleFonts.quicksand(
+                        textStyle: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Color(0x110061ff),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 44.0,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                      padding: EdgeInsets.only(
+                          left: 21.0, top: 27.0, right: 33.0, bottom: 39.0),
+                      width: 319,
+                      height: 166,
+                      child: Column(
+                        children: [
+                          Text(
+                            "Elon Musk is planning a 24-hour Tesla restaurant in Hollywood",
+                            style: GoogleFonts.quicksand(
+                                textStyle: TextStyle(
+                              color: Color(0xffFFFFFF),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            )),
+                          ),
+                          Text('Kate')
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                          color: Color(0xff011b45))),
+                ),
+                SizedBox(
+                  height: 27.0,
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                      padding: EdgeInsets.only(
+                          left: 42.0, top: 27.0, right: 33.0, bottom: 39.0),
+                      width: 319,
+                      height: 166,
+                      child: Text(
+                        "Elon Musk is planning a 24-hour Tesla restaurant in Hollywood",
+                        style: TextStyle(
+                          color: Color(0xffFFFFFF),
+                          fontSize: 20,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(32),
+                          color: Color(0xff0665ff))),
+                ),
               ],
             ),
           ),

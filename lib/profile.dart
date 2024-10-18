de5dd800091242e3bd2dd67b2c8fd5f154e80f73
Nancy Chizea <chizeanancy@gmail.com>
2024-10-18 19:08:59 +0100

    Commence Profile Page

--- lib/profile.dart
+++ lib/profile.dart
@@ -1,3 +1,4 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 
 class Profile extends StatelessWidget {
@@ -5,6 +6,30 @@ class Profile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        backgroundColor: Color(0xffF5F8FA),
+        body: SingleChildScrollView(
+          child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SafeArea(
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                        "Profile",
+                        style: GoogleFonts.quicksand(
+                          textStyle: const TextStyle(
+                              fontSize: 36,
+                              fontWeight: FontWeight.w600,
+                              height: 45 / 36,
+                              color: Color(0xff011B45)),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ));
   }
 }

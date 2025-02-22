7c15e6bf6dac7cea367e444930ecffc22a95a60e
Nancy Chizea <chizeanancy@gmail.com>
2025-02-22 20:32:04 +0100

    Complete First Profile  Banner

--- lib/profile.dart
+++ lib/profile.dart
@@ -28,31 +28,126 @@ class Profile extends StatelessWidget {
                 ),
                 SizedBox(height: 25.0),
                 Align(
-                    alignment: Alignment.topLeft,
-                    child: Container(
-                      padding: const EdgeInsets.all(16.0),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(32),
-                        color: Color(0xff0665FF),
-                        //  Align(
-                        //   alignment: Alignment.centerLeft,
-                      ),
-                      child: Row(
-                        children: [
-                          CircleAvatar(
-                            radius: 45,
-                            backgroundImage: AssetImage('assets/avatar.jpeg'),
-                          ),
-                          Text('Nancy Chizea', style: GoogleFonts.quicksand(
-                      textStyle: const TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.w600,
-                          height: 45 / 36,
-                          color: Colors.white),
-                    ),),
-                        ],
-                      ),
-                    )),
+                  alignment: Alignment.topLeft,
+                  child: Container(
+                    padding: const EdgeInsets.all(16.0),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(32),
+                      color: Color(0xff0665FF),
+                      //  Align(
+                      //   alignment: Alignment.centerLeft,
+                    ),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            CircleAvatar(
+                              radius: 45,
+                              backgroundImage: AssetImage('assets/avatar.jpeg'),
+                            ),
+                            SizedBox(width: 21.0),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              verticalDirection: VerticalDirection.down,
+                              children: [
+                                Text(
+                                  'Nancy Chizea',
+                                  style: GoogleFonts.quicksand(
+                                    textStyle: const TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w600,
+                                        height: 45 / 36,
+                                        color: Colors.white),
+                                  ),
+                                ),
+                                SizedBox(height: 9.0),
+                                Container(
+                                    padding: const EdgeInsets.fromLTRB(
+                                        7.0, 2.0, 7.0, 1.0),
+                                    decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(5),
+                                        color: Colors.white),
+                                    child: Text(
+                                      'PRO',
+                                      style: TextStyle(
+                                        color: Color(0xff0665FF),
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ))
+                              ],
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 21.0),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                             Expanded(
+                              child: Container(
+                                  padding: const EdgeInsets.fromLTRB(
+                                      22.0, 6.0, 21.0, 7.0),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(6),
+                                      color: Colors.white.withOpacity(0.1)),
+                                  child: Text.rich(
+                                    TextSpan(
+                                      text: '28\n',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                      children: [
+                                        TextSpan(
+                                          text: 'Days Streak',
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                      ],
+                                    ),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 50,
+                                    ),
+                                  )),
+                            ), SizedBox(width: 10.0),
+                            Expanded(
+                              child: Container(
+                                  padding: const EdgeInsets.fromLTRB(
+                                      22.0, 6.0, 21.0, 7.0),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(6),
+                                      color: Colors.white.withOpacity(0.1)),
+                                  child: Text.rich(
+                                    TextSpan(
+                                      text: '200\n',
+                                      style: TextStyle(
+                                        fontSize: 24,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                      children: [
+                                        TextSpan(
+                                          text: 'Days Streak',
+                                          style: TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w400),
+                                        ),
+                                      ],
+                                    ),
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 50,
+                                    ),
+                                  )),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ]),
             ),
           ),
